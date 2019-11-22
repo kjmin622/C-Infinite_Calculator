9a4151a66188652567ed6a4659ac7898110c22ea
kjmin622 <kjmin622@naver.com>
2019-11-22 23:55:44 +0900

    main

--- Main.c
+++ Main.c
@@ -31,7 +31,6 @@ int main(void){
             p->prev = exp;
             exp = p;
         }
-        print_list(exp_head);
         ////////////////////////////////////////////////
         //수정부
         if(exp_head->next != NULL && exp_head->d == '-'){ //맨 앞 - 나오는 경우
@@ -77,89 +76,32 @@ int main(void){
         LINK input_link;
         situration = 0;
 
+        
         for(; exp != NULL; exp = exp->next){
-            if(exp->d >= '0' && exp->d <= '9'){ //피연산자 스택 넣기
+            if((exp->d == '.') || (exp->d >= '0' && exp->d <= '9')){ //피연산자 스택 넣기
                 if(situration == 0){
                     rational = char_to_list('+');
                     insert(rational, exp->d);
-                    input_link = rational;
+                    input_link = rational->next;
                     situration = 1;
                 }
                 else{
                     insert(input_link,exp->d);
                     input_link = input_link->next;
                 }
-                if(exp->next->d < '0' || exp->next->d > '9'){
+                if(exp->next == NULL || (exp->next->d < '0' || exp->next->d > '9') && (exp->next->d != '.')){
                     Push(&num_stack,rational);
                     situration = 0;
                 }
             }
-            else{
-                if(exp->d == ')'){
-                    ;
-                }
-                else if(exp->d == '('){
-                    Push(&oper_stack,char_to_list(exp->d));
-                }
-                else{
-                    if(exp->d == '+' || exp->d == '-'){
-                        while(!IsEmpty(&oper_stack)){
-                            if(Top(&oper_stack)->d == '('){
-                                free_all(Pop(&oper_stack));
-                                break;
-                            }
-                            else{
-                                LINK p2 = Pop(&num_stack);
-                                LINK p1 = Pop(&num_stack);
-                                LINK oper = Pop(&oper_stack);
-                                LINK p3 = calculator(p1,p2,oper->d);
-                                free_all(p2); free_all(p1); free_all(oper);
-                                Push(&num_stack,p3);
-                            }
-                        }
-                        Push(&oper_stack,char_to_list(exp->d));
-                    }
-                    /*
-                    else{
-                        while(!IsEmpty(&oper_stack) && Top(&oper_stack)->d != '+' && Top(&oper_stack)->d != '-'){
-                            if(Top(&oper_stack)->d == '('){
-                                free_all(Pop(&oper_stack));
-                                break;
-                            }
-                            else{
-                                LINK p2 = Pop(&num_stack);
-                                LINK p1 = Pop(&num_stack);
-                                LINK oper = Pop(&oper_stack);
-                                LINK p3 = calculator(p1,p2,oper->d);
-                                free_all(p2); free_all(p1); free_all(oper);
-                                Push(&num_stack,p3);
-                            }
-                        }
-                        Push(&oper_stack,char_to_list(exp->d));
-                    }
-                    */
-                }
-            }
+            
         }
-
-        while(!IsEmpty(&oper_stack)){
-            LINK p2 = Pop(&num_stack);
-            LINK p1 = Pop(&num_stack);
-            LINK oper = Pop(&oper_stack);
-            char operation = oper->d;
-            
-            Push(&num_stack, calculator(p1,p2,operation));
-
-            free_all(p2); free_all(p1); free_all(oper);
+        
+        while(!IsEmpty(&num_stack)){
+            LINK output = Pop(&num_stack);
+            print_list(output);
+            free(output);
         }
-
-        LINK answer = Pop(&num_stack);
-        print_list(answer);
-        free(answer);
-
-            
-            
-
         ////////////////////////////////////////////////
         //메모리를 아끼자
         free_all(exp_head);
