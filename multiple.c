35ee47802c7ad58249a462d53211c3f2ea7d0760
huskycat1202 <annie991202@naver.com>
2019-11-25 20:23:45 +0900

    multi

--- multiple.c
+++ multiple.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include<stdlib.h>
 #include "inter_link.h"
 // 0 1 2 3 4 5 6 7 8
 // + 1 3 5 . 3 2 1
@@ -10,51 +12,40 @@ LINK multiple(LINK num1, LINK num2){
 
     // 계산 값 부호 결정 
     //
-    if(num1->d == num2->d ){
-        ans->d = '+';}
-    else{
-        ans->d = '-';}
+    if(num1->d == num2->d) ans->d = '+';
+    else                   ans->d = '-';
 
     // 반대로 뒤집기 
     // 123.531+
     // 9712.5-
 
-    LINK num1_copy = last_link(num1);
-    LINK num2_copy = last_link(num2);
+    LINK num1_last = last_link(num1);  // num1_last : num1의 마지막   //   num2_last : num2의 마지막 
+    LINK num2_last = last_link(num2);
 
     // 소수점 계산 
     //
     int point = 0;
-    for( ; num1_copy->d != '.' ; num1_copy = num1_copy->prev){point++};//3
-    for( ; num2_copy->d != '.' ; num2_copy = num2_copy->prev){point++};//3+4=7
-
-
-
-
-    for( ; num2_copy->d != NULL ; num2_copy = num2_copy->next);
-        for( ; num2_copy->d != '+' || num2_copy != '-' ; num2_copy = num2_copy->prev){
-            num2_reverse = num2_copy;}
-
-
-
-    // 소수점 계산 
-    int point1 =0, point2 =0;
-
-     for( ; num1_copy->d != NULL ; num1_copy = num1_copy->next);
-     for( ; num1_copy->d != '.' ; num1_copy = num1_copy->prev) point++;
-
-
-    // 소수점 위치 계산 
-    //
-    LINK num1_point = num1;
-    LINK num2_point = num2;
-
+    LINK num_copy = num1_last;
+    for( ; num_copy->d != '.' ; num_copy = num_copy->prev) point++;//3
     
-    for( ; num1_point->d != '.' ; num1_point=num1_point->next); //3
-    for( ; num2_point->d != '.' ; num2_point=num2_point->next); //2
+    num_copy = num2_last;
+    for( ; num_copy->d != '.' ; num_copy = num_copy->prev) point++;//3+4=7
     
+    //printf("%d",point);
+
     // 점 빼기
-    //
+    // 0 1 2 3 4 5 6 7 8
+    // + 1 3 5 . 3 2 1
+    // - 5 . 2 1 7 9
+
+
+    LINK num1_int = copy_link(num1->next);
+    
+
+    //print_list(num1_int);
+    //print_list(num2_int);
+
+    
 
     // 계산
     // num2->d* num1->d .. 해서 계산 후 9보다 크거나 같으면 다음칸에 올림
@@ -67,6 +58,30 @@ LINK multiple(LINK num1, LINK num2){
     // 소수점 계산 한 값만큼 소수점 뒤에서부터 카운트
     //
 
+}
 
-       
+int main(){
+    LINK a1, a2;
+    char input;
+
+    scanf(" %c",&input);
+    a1 = char_to_list(input);
+    LINK a_input = a1;
+    while(scanf("%c",&input)){
+        if(input == '\n') break;
+        insert(a_input,input);
+        a_input = a_input->next;
+    }
+
+    scanf(" %c",&input);
+    a2 = char_to_list(input);
+    a_input = a2;
+    while(scanf("%c",&input)){
+        if(input == '\n') break;
+        insert(a_input,input);
+        a_input = a_input->next;
+    }
+
+    LINK a3 = multiple(a1,a2);
+}
 
